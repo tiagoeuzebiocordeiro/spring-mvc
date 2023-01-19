f5b8932e8810cea667e657d610bad872eddd7dfd
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2023-01-19 15:08:44 -0300

    Continuacao do Desenvolvimento - CRUD

--- src/main/java/curso/springmvc/controller/PessoaController.java
+++ src/main/java/curso/springmvc/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springmvc.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springmvc.model.Pessoa;
@@ -16,16 +22,19 @@ public class PessoaController {
 	PessoaRepository pessoaRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoas = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoas);
+		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
 	
@@ -34,7 +43,40 @@ public class PessoaController {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+
 		return andView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		andView.addObject("pessoaobj", pessoa.get());
+		return andView;
+	}
+	
+	
+	@GetMapping("/excluirpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+		
+		pessoaRepository.deleteById(idpessoa);
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.findAll());
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+	
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.buscaPessoaPeloNome(nomepesquisa));
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+		
 	}
 	
 }
--- src/main/java/curso/springmvc/repository/PessoaRepository.java
+++ src/main/java/curso/springmvc/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package curso.springmvc.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,7 @@ import jakarta.transaction.Transactional;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> buscaPessoaPeloNome(String nome);
+	
 }
